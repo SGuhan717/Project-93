b0fd42c45b173ca3bbbeff71465f53d5f9d9245a
SGuhan717 <83213442+SGuhan717@users.noreply.github.com>
2021-12-24 08:18:22 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,31 +1,35 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyAEKYcTMOSOAkfkYDF4Y_6LuYKTv1tjD7k",
-    authDomain: "project-93-32f1d.firebaseapp.com",
-    databaseURL: "https://project-93-32f1d-default-rtdb.firebaseio.com",
-    projectId: "project-93-32f1d",
-    storageBucket: "project-93-32f1d.appspot.com",
-    messagingSenderId: "1088653578706",
-    appId: "1:1088653578706:web:e2fb3f0e153d977cd8f80b"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDOY4FGNP7r1s844IGEqS1sMOoIK12wooc",
+  authDomain: "project93-5ad5f.firebaseapp.com",
+  databaseURL: "https://project93-5ad5f-default-rtdb.firebaseio.com",
+  projectId: "project93-5ad5f",
+  storageBucket: "project93-5ad5f.appspot.com",
+  messagingSenderId: "499847069342",
+  appId: "1:499847069342:web:1e6853f45ab4c97b1b458f"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-    user_name = localStorage.getItem("Username");
-    document.getElementById("welcome_user_name").innerHTML = "Welcome "+user_name+"!";
+  user_name = localStorage.getItem("Username");
+  document.getElementById("welcome_user_name").innerHTML = "Welcome "+user_name+"!";
 
-    function addroom() {
-           room_name = document.getElementById("room_name").value;
-  
-          localStorage.setItem("Roomname",room_name);
-      
-          window.location = "kwitter_page.html";
-    }
+  function addroom() {
+         room_name = document.getElementById("room_name").value;
+
+        localStorage.setItem("Roomname",room_name);
+    
+        window.location = "kwitter_page.html";
+
+        firebase.database().ref("/").child(room_name).update({
+              purpose: "Adding Room Name"
+        });
+  }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-     Room_names = childKey;
-    //Start code
+   Room_names = childKey;
+  //Start code
 
-    //End code
-    });});}
+  //End code
+  });});}
 getData();
